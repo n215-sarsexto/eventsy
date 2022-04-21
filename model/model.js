41340072f6c92ef9d434894686f4034c95441e2c
Summer S <snowykittin@gmail.com>
2022-04-21 00:07:37 -0400

    Finish home, about, and gallery item pages

--- model/model.js
+++ model/model.js
@@ -98,7 +98,25 @@ var homeContent = `<div class="hero-image">
   </div>
 </div>
 </div>`;
-var aboutContent = `<h1>ABOUT</h1>`;
+var aboutContent = `<div class="about-content">
+<div class="header-image">
+  <h6>OUR HISTORY:</h6>
+</div>
+<hr />
+<div class="about">
+  <p>
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae vitae
+    dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+    aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+    eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+    qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+    sed quia non numquam eius modi tempora incidunt ut labore et dolore
+    magnam aliquam quaerat voluptatem.
+  </p>
+</div>
+</div>`;
 var galleryContent = ``;
 var blogContent = ``;
 var contactContent = ``;
